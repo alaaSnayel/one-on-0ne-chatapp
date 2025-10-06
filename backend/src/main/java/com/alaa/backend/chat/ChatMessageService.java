fd11ba82291d5be3a7a8c69665ab37cea68c60a9
alaaSnayel <alaanayel111@gmail.com>
2025-10-06 10:04:11 +0300

    Backend: Implement the ChatMessage service

--- backend/src/main/java/com/alaa/backend/chat/ChatMessageService.java
+++ backend/src/main/java/com/alaa/backend/chat/ChatMessageService.java
@@ -1,5 +1,36 @@
 package com.alaa.backend.chat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.alaa.backend.chatroom.ChatRoomService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ChatMessageService {
 
+  private final ChatMessageRepository chatMessageRepository;
+  private final ChatRoomService chatRoomService;
+
+  public ChatMessage save(ChatMessage chatMessage) {
+    var chatRoom = chatRoomService.getChatRoomId(
+      chatMessage.getSenderId(),
+      chatMessage.getRecipientId(),
+      true
+      ).orElseThrow();
+    chatMessage.setChatId(chatRoom);
+    chatMessageRepository.save(chatMessage);
+    return chatMessage;
+  }
+
+  public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+    var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+
+    return chatId.map(chatMessageRepository::findByChatId).orElse(new ArrayList<>());
+
+  }
 }
